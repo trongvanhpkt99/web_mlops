8b0eecf413170f6e9ebb64a20208a10d2f43222c
trongvanhpkt99 <trongvanhpkt99@gmail.com>
2022-06-07 02:12:03 +0700

    update FE

--- app.py
+++ app.py
@@ -3,17 +3,18 @@ import os
 from PIL import Image
 from werkzeug.utils import secure_filename
 import torch
-from flask import Flask,session, render_template,flash, request, redirect, url_for
+from flask import send_file,Flask,session, render_template,flash, request, redirect, url_for
 import datetime
 import json
 import zipfile
 
+HOST='0.0.0.0'
 app = Flask(__name__)
 
 uploads_dir = os.path.join(os.getcwd(), 'projects')
 
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
-def create_project(project_name):
+def create_project(project_name,url):
     dir=os.path.join(os.getcwd(), 'projects')+'/'+project_name
     os.mkdir(dir)
     os.mkdir(dir+'/'+'data')
@@ -23,6 +24,8 @@ def create_project(project_name):
     }
     with open(dir+"/info.json", "w") as outfile:
         json.dump(info, outfile)
+    open(dir+'/'+project_name+'.yaml','wb').write(open("./sample.yaml", "rb").read())
+    open(dir+'/data/'+project_name+'.yaml','a').write("\ndownload: "+url+url_for('downloadFile',project=project_name,file=project_name+'.zip'))
 def get_dirr():
     return os.path.join(os.getcwd(), 'projects')
 def get_project():
@@ -31,15 +34,16 @@ def get_project():
 @app.route("/home", methods=["GET", "POST"])
 def home():
     if request.method == 'POST':
-        
+        url=request.base_url.replace('/home','')
         new_pj=request.form.get("pj_name")
-        create_project(new_pj)
+        create_project(new_pj,url)
             
     return render_template("homepage.html",projects=get_project())
 @app.route("/project", methods=["GET", "POST"])
 def project():
     session["project"]=request.args.get('project')
-    return render_template("project.html",dirr=get_dirr())
+    data=open(get_dirr()+'/'+session["project"]+'/data/'+session["project"]+'.yaml','r',encoding='utf-8').read()
+    return render_template("project.html",data=data)
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
    if request.method == 'POST':
@@ -48,10 +52,19 @@ def upload_file():
             x=os.path.join(uploads_dir,session["project"],'data', secure_filename(f.filename))
             print(x)
             f.save(os.path.join(uploads_dir,session["project"],'data', secure_filename(f.filename)))
-            with zipfile.ZipFile(os.path.join(uploads_dir,session["project"],'data', secure_filename(f.filename)),"r") as zip_ref:
-                zip_ref.extractall(os.path.join(uploads_dir,session["project"],'data'))
         return (redirect(url_for('home',projects=get_project())))
+@app.route('/download', methods=['GET'])
+def downloadFile ():
+    project=request.args.get("project")
+    file=request.args.get("file")
+    # return uploads_dir+'/'+project+'/'+file
+    return send_file(uploads_dir+'/'+project+'/data/'+file,attachment_filename =file,as_attachment=True)
 
+@app.route('/trainconfig', methods=["GET", "POST"])
+def pj_config():
+    if request.method == 'POST':
+        pass 
+    return render_template('project_config.html')
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Flask app exposing yolov5 models")
     parser.add_argument("--port", default=5000, type=int, help="port number")
@@ -59,4 +72,4 @@ if __name__ == "__main__":
     app.secret_key = 'secret key'
     app.config['SESSION_TYPE'] = 'filesystem'
 
-    app.run(host="0.0.0.0",port=8080,debug=True)  # debug=True causes Restarting with stat
+    app.run(host=HOST,port=5055,debug=True)
